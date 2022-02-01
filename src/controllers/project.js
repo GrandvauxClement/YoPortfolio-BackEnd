a131281ca74bedae8c5f8fe33ae61b034798b023
Simon Grandvaux <clement.grandvaux@hotmail.com>
2022-02-01 15:08:42 +0100

    define route getAll + getOne

--- src/controllers/project.js
+++ src/controllers/project.js
@@ -9,3 +9,29 @@ exports.createProject = (req, res, next) => {
         .then(() => res.status(201).json({message: 'Project créé correctement ! :)'}))
         .catch(error => res.status(400).json({error}));
 };
+
+exports.getOneProject = (req, res, next) => {
+    Project.findOne({
+        _id: req.params._id
+    }).then(
+        (project)=> {
+            res.status(200).json(project);
+        }
+    ).catch(
+        (error) => {
+            res.status(500).json({error});
+        }
+    );
+};
+
+exports.getAllProject = (req, res, next) => {
+    Project.find().then(
+        (projects) => {
+            res.status(200).json(projects);
+        }
+    ).catch(
+        (error) => {
+            res.status(500).json({error});
+        }
+    );
+};
--- src/routes/project.js
+++ src/routes/project.js
@@ -2,4 +2,8 @@ const express = require('express');
 const projectController = require('../controllers/project');
 const router = express.Router();
 
+router.get('/', projectController.getAllProject);
 router.post('/', projectController.createProject);
+router.get('/:id', projectController.getOneProject);
+
+module.exports = router;
