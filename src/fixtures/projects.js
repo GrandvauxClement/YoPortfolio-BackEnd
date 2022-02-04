d37c48cd0d2a6504f0c2a718992dad5ff763c434
GrandvauxClement <clement.grandvaux@hotmail.com>
2022-02-04 16:55:50 +0100

    add fake fixtures project

--- src/fixtures/projects.js
+++ src/fixtures/projects.js
@@ -22,7 +22,79 @@ module.exports = [
             "officia deserunt mollit anim id est laborum",
         type: "Identité de marque",
         images: ['logo1.jpg', 'logo2.jpg', 'logo3.jpg', 'logo4.jpg'],
-        principalImage: "print1.jpg",
+        principalImage: "logo1.jpg",
+        videoLink: "https://www.youtube.com/watch?v=LH5ay10RTGY",
+    },
+    {
+        title: "Création dépliant dartagnant",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut " +
+            "labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut " +
+            "aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum " +
+            "dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui " +
+            "officia deserunt mollit anim id est laborum",
+        type: "Print",
+        images: ['print1.jpg', 'print2.jpg', 'print3.jpg', 'print4.jpg'],
+        principalImage: "print2.jpg",
+        videoLink: "https://www.youtube.com/watch?v=LH5ay10RTGY",
+    },
+    {
+        title: "Identité visuel GlouGlou market",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut " +
+            "labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut " +
+            "aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum " +
+            "dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui " +
+            "officia deserunt mollit anim id est laborum",
+        type: "Identité de marque",
+        images: ['logo1.jpg', 'logo2.jpg', 'logo3.jpg', 'logo4.jpg'],
+        principalImage: "logo2.jpg",
+        videoLink: "https://www.youtube.com/watch?v=LH5ay10RTGY",
+    },
+    {
+        title: "Création dépliant dartagnant",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut " +
+            "labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut " +
+            "aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum " +
+            "dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui " +
+            "officia deserunt mollit anim id est laborum",
+        type: "Print",
+        images: ['print1.jpg', 'print2.jpg', 'print3.jpg', 'print4.jpg'],
+        principalImage: "print3.jpg",
+        videoLink: "https://www.youtube.com/watch?v=LH5ay10RTGY",
+    },
+    {
+        title: "Identité visuel GlouGlou market",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut " +
+            "labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut " +
+            "aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum " +
+            "dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui " +
+            "officia deserunt mollit anim id est laborum",
+        type: "Identité de marque",
+        images: ['logo1.jpg', 'logo2.jpg', 'logo3.jpg', 'logo4.jpg'],
+        principalImage: "logo3.jpg",
+        videoLink: "https://www.youtube.com/watch?v=LH5ay10RTGY",
+    },
+    {
+        title: "Création dépliant dartagnant",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut " +
+            "labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut " +
+            "aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum " +
+            "dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui " +
+            "officia deserunt mollit anim id est laborum",
+        type: "Print",
+        images: ['print1.jpg', 'print2.jpg', 'print3.jpg', 'print4.jpg'],
+        principalImage: "print4.jpg",
+        videoLink: "https://www.youtube.com/watch?v=LH5ay10RTGY",
+    },
+    {
+        title: "Identité visuel GlouGlou market",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut " +
+            "labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut " +
+            "aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum " +
+            "dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui " +
+            "officia deserunt mollit anim id est laborum",
+        type: "Identité de marque",
+        images: ['logo1.jpg', 'logo2.jpg', 'logo3.jpg', 'logo4.jpg'],
+        principalImage: "logo4.jpg",
         videoLink: "https://www.youtube.com/watch?v=LH5ay10RTGY",
     },
 ];
