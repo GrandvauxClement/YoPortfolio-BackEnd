ac9188b6003c702ff739cb7ba5fa3ba7bef4a6c2
GrandvauxClement <clement.grandvaux@hotmail.com>
2022-03-14 12:09:20 +0100

    protected route login feature is good

--- src/routes/message.js
+++ src/routes/message.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const messageController = require('../controllers/message');
 const router = express.Router();
+const verifyJwt = require("../middleware/verifyJWT");
 
-router.get('/', messageController.getAllMessage);
+router.get('/', verifyJwt, messageController.getAllMessage);
 router.post('/', messageController.createMessage);
-router.get('/:id', messageController.getOneMessage);
-router.delete('/:id', messageController.deleteProject);
+router.get('/:id', verifyJwt, messageController.getOneMessage);
+router.delete('/:id',verifyJwt, messageController.deleteProject);
 
 module.exports = router;
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const loginController = require('../controllers/login');
 const router = express.Router();
+const verifyJwt = require("../middleware/verifyJWT");
 
 router.post('/', loginController.login);
+router.get('/checkToken', verifyJwt, loginController.checkValidityToken);
 
 module.exports = router;
