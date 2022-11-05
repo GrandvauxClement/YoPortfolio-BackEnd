e2f8dd99013c60590b17a258da13706a44869663
GrandvauxClement <clement.grandvaux@hotmail.com>
2022-11-05 15:40:40 +0100

    test

--- src/app/app.js
+++ src/app/app.js
@@ -10,7 +10,7 @@ const MessageRoutes = require('../routes/message');
 const ServiceRoutes = require('../routes/service');
 const AuthRoute = require('../routes/user');
 const mongoose = require('mongoose');
-require("dotenv").config();
+// require("dotenv").config();
 const app = express();
 mongoose
     // connect mongoose to the database mongodb+srv://admin:3TmfTX1cK7Y6rCz3@cluster0.n5wob.mongodb.net/test   `mongodb://localhost/yoPortfolio`
