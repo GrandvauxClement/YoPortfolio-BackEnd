ca914ffa19dc96d1c889d2b295b42d2a61f828c3
GrandvauxClement <clement.grandvaux@hotmail.com>
2022-11-16 18:50:33 +0100

    update env variable name

--- src/controllers/project.js
+++ src/controllers/project.js
@@ -3,8 +3,8 @@ const fs = require("fs");
 const aws = require("aws-sdk");
 
 const s3 = new aws.S3({
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+    accessKeyId: process.env.YO_AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.YO_AWS_SECRET_ACCESS_KEY
 });
 
 const uploadFile = (fileName) => {
