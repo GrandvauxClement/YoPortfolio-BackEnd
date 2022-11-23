512e10b5fdf7ec79337609716287912f3555f724
GrandvauxClement <clement.grandvaux@hotmail.com>
2022-11-23 21:09:08 +0100

    update config vercel

--- src/app/app.js
+++ src/app/app.js
@@ -36,7 +36,7 @@ mongoose
         app.get("/", (req, res) => {
             res.setHeader('Access-Control-Allow-Origin', '*');
             res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-            res.setHeader('Access-Control-Allow-Credentials', true);
+            res.setHeader('Access-Control-Allow-Credentials', 'true');
             res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, OPTIONS, DELETE');
 
 //---- other code
@@ -46,7 +46,10 @@ mongoose
                     body: "OK"
                 }))
             }else {
-                res.send("Hello World!");
+                return res.status(200).json(({
+                    body: "OK"
+                }))
+               // res.send("Hello World!");
             }
 
         });
